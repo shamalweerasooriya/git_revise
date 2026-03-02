1dff57e031ed62176ae8ad4cd926ea23276600b9
shamal <shamal.weerasooriya@pagero.com>
2026-03-02 12:48:25 +0530

    multiply

--- mandara.py
+++ mandara.py
@@ -11,3 +11,13 @@ def subtract(a, b):
     return a - b
 
 print(subtract(1, 2))
+
+def multiply(a, b):
+    return a * b
+
+print(multiply(1, 2))
+
+def divide(a, b):
+    return a / b
+
+print(divide(1, 2))
